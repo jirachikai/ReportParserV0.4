e9ab09f00163b5ca7cff606fab8e52937d096227
jirachikai <wenkai.mo.1992@gmail.com>
2013-10-30 14:09:30 +0800

    Add Logging into database

--- ReportParser/DataBase/DBReader.py
+++ ReportParser/DataBase/DBReader.py
@@ -3,62 +3,83 @@ import sqlite3
 from ..Model import Test
 from datetime import *
 import socket
+import logging
 class DBReader(object):
 	def ReadSingleDay(self,date,productName):
-		conn = sqlite3.connect("Result.db")
-		cursor = conn.execute('SELECT DT_ID,Link,DocumentName,Type FROM DailyTest WHERE Date=? and productName=?',(date,productName,))
-		docs = []
-		for line in cursor.fetchall():
-			doc = Test.Document()
-			doc.DocumentTime = date
-			doc.Link = line[1]
-			doc.DocumentName = line[2]
-			doc.DocumentType = line[3]
-			doc.Test = self.__readDocumentTest(conn,line[0]) #line[0] is dailyTestID
-			docs.append(doc)
-		conn.close()
+		try:
+			docs = []
+			conn = sqlite3.connect("Result.db")
+			cursor = conn.execute('SELECT DT_ID,Link,DocumentName,Type FROM DailyTest WHERE Date=? and productName=?',(date,productName,))	
+			for line in cursor.fetchall():
+				doc = Test.Document()
+				doc.DocumentTime = date
+				doc.Link = line[1]
+				doc.DocumentName = line[2]
+				doc.DocumentType = line[3]
+				doc.Test = self.__readDocumentTest(conn,line[0]) #line[0] is dailyTestID
+				docs.append(doc)
+			conn.close()
+		except Exception as err:
+			self.__writeLog("Logging.log","FunctionReadSingleDay: " + str(err)) 
 		return docs
 	
 	def ReadReportDate(self,date,productName):
-		date = ''.join(date.split('-'))#change Year-Month-Day to YearMonthDay for DB Search
-		date = date + '%'
-		conn = sqlite3.connect("Result.db")
-		cursor = conn.execute('SELECT Date FROM DailyTest WHERE Date LIKE ? and ProductName=?',(date,productName,))
-		list = cursor.fetchall()
-		l = [i[0] for i in list]
+		try:
+			l=[]
+			date = ''.join(date.split('-'))#change Year-Month-Day to YearMonthDay for DB Search
+			date = date + '%'
+			conn = sqlite3.connect("Result.db")
+			cursor = conn.execute('SELECT Date FROM DailyTest WHERE Date LIKE ? and ProductName=?',(date,productName,))
+			list = cursor.fetchall()
+			l = [i[0] for i in list]
+		except Exception as err:
+			self.__writeLog("Logging.log","FunctionReadSingleDay: " + str(err))
 		if l:
 			return max(l)
 		else:
 			return None
 	
 	def ReadPeriodDays(self,start,end,productName):
-		conn = sqlite3.connect("Result.db")
-		sd = self.__getDate(start)
-		ed = self.__getDate(end)
-		tempDate = sd
-		if (ed - sd).days < 0 or (ed - sd).days > 31:
-			return None
-		d = []
-		total = []
-		passed = []
-		link = []
-		ip = socket.gethostbyname(socket.gethostname())
-		while tempDate <= ed:
-			DateString = self.__getDateString(tempDate)
-			cursor = conn.execute('SELECT Total,Passed FROM BirdsView WHERE Date=? and productName=?',(DateString,productName,))
-			line = cursor.fetchone()
-			d.append(tempDate.strftime("%d") )
-			link.append('/'+productName+'/Date/' + tempDate.strftime("%Y-%m-%d"))
-			if not line:
-				total.append(0)
-				passed.append(0)
+		try:
+			conn = sqlite3.connect("Result.db")
+			sd = self.__getDate(start)
+			ed = self.__getDate(end)
+			tempDate = sd
+			if (ed - sd).days < 0 or (ed - sd).days > 31:
+				return None
+			d = []
+			total = []
+			passed = []
+			link = []
+			ip = socket.gethostbyname(socket.gethostname())
+			while tempDate <= ed:
+				DateString = self.__getDateString(tempDate)
+				cursor = conn.execute('SELECT Total,Passed FROM BirdsView WHERE Date=? and productName=?',(DateString,productName,))
+				line = cursor.fetchone()
+				d.append(tempDate.strftime("%d") )
+				link.append('/'+productName+'/Date/' + tempDate.strftime("%Y-%m-%d"))
+				if not line:
+					total.append(0)
+					passed.append(0)
+					tempDate += timedelta(days = 1,seconds =0, microseconds = 0)
+					continue
+				total.append(line[0])
+				passed.append(line[1])
 				tempDate += timedelta(days = 1,seconds =0, microseconds = 0)
-				continue
-			total.append(line[0])
-			passed.append(line[1])
-			tempDate += timedelta(days = 1,seconds =0, microseconds = 0)
+		except Exception as err:
+			self.__writeLog("Logging.log","FunctionReadSingleDay: " + str(err))
+			return [0,0,0,0]
 		return total,passed,d,link
-
+	
+	def __writeLog(self,filePath,errorMessage):
+		logger = logging.getLogger("database")
+		formatter = logging.Formatter('%(name)-12s %(asctime)s %(levelname)-8s %(message)s', '%a, %d %b %Y %H:%M:%S',) 
+		file_handler = logging.FileHandler(filePath) 
+		file_handler.setFormatter(formatter) 
+		logger.addHandler(file_handler)  
+		logger.error(errorMessage)
+		logger.removeHandler(file_handler)  
+		
 	def __getDate(self,d):
 		splitDate = d.split('-')
 		return date(int(splitDate[0]),int(splitDate[1]),int(splitDate[2]))
--- ReportParser/DataBase/DBWriter.py
+++ ReportParser/DataBase/DBWriter.py
@@ -1,24 +1,41 @@
 import sqlite3
+import logging
 DBPath = "Result.db"
 class DBWriter(object):
 	def WriteSingleDay(self,doc,productName):
-		DT_ID = self.__writeDailyTestTable(doc,productName)
-		self.__writeTest(doc,DT_ID)
-
+		try:
+			DT_ID = self.__writeDailyTestTable(doc,productName)
+			self.__writeTest(doc,DT_ID)
+		except Exception as err:
+			self.__writeLog("Logging.log","WriteSingleDay: " + str(err))
+			
 	def WriteBirdsView(self,date,total,passes,productName):
-		conn = sqlite3.connect(DBPath)
-		cursor = conn.execute('SELECT Date FROM BirdsView WHERE Date=? and productName=?',(date,productName,))
-		line = cursor.fetchone()
-		if line:
-			conn.execute('UPDATE BirdsView SET Total = ?, Passed = ? WHERE Date = ? and ProductName = ?',(total,passes,date,productName))
+		try:
+			conn = sqlite3.connect(DBPath)
+			cursor = conn.execute('SELECT Date FROM BirdsView WHERE Date=? and productName=?',(date,productName,))
+			line = cursor.fetchone()
+			if line:
+				conn.execute('UPDATE BirdsView SET Total = ?, Passed = ? WHERE Date = ? and ProductName = ?',(total,passes,date,productName))
+				conn.commit()
+				conn.close()
+				return 
+			list = [date,total,passes,productName]
+			conn.execute('INSERT INTO BirdsView VALUES(?,?,?,?)',list)
 			conn.commit()
 			conn.close()
-			return 
-		list = [date,total,passes,productName]
-		conn.execute('INSERT INTO BirdsView VALUES(?,?,?,?)',list)
-		conn.commit()
-		conn.close()
-
+		except Exception as err:
+			self.__writeLog("Logging.log","WriteBirdsView: " + str(err))
+			conn.close()
+	
+	def __writeLog(self,filePath,errorMessage):
+		logger = logging.getLogger("database")
+		formatter = logging.Formatter('%(name)-12s %(asctime)s %(levelname)-8s %(message)s', '%a, %d %b %Y %H:%M:%S',) 
+		file_handler = logging.FileHandler(filePath) 
+		file_handler.setFormatter(formatter) 
+		logger.addHandler(file_handler)  
+		logger.error(errorMessage)
+		logger.removeHandler(file_handler) 
+		
 	def __writeDailyTestTable(self,doc,productName):
 		conn = sqlite3.connect(DBPath)
 		list = [doc.DocumentTime,self.__getLink(doc.Link),doc.DocumentName,doc.DocumentType,productName]
